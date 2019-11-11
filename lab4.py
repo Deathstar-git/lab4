699a2a838fb5b484e9aba895f4c13276b863adce
Deathstar-git <79025632112tch@gmail.com>
2019-11-12 02:31:22 +0800

    Working version with exception

--- lab4.py
+++ lab4.py
@@ -3,7 +3,7 @@ from random import randint
 
 def main():
     a = []
-    N = input("Введите длинну списка для сортировки:")
+    N = input("Введите длину списка для сортировки:")
     try:
         Val = int(N)
     except TypeError:
@@ -16,7 +16,7 @@ def main():
             create_list(a, N)
             bubble_sort(a, N)
         else:
-            print("Для сортировки списка укажите длинну списка больше 1.")
+            print("Для сортировки списка укажите длину списка больше 1.")
 
 
 def create_list(a, N):
@@ -26,6 +26,16 @@ def create_list(a, N):
 
 
 def bubble_sort(a, N):
+    """Sorted list
+    >>> bubble_sort([50, 19, 5], 3)
+    [5, 19, 50]
+    >>> bubble_sort([14, 3, 67, 38, 79], 5)
+    [3, 14, 38, 67, 79]
+    >>> bubble_sort([58, 47, 2, 15, 24, 67], 7)
+     Traceback (most recent call last):
+       ...
+     IndexError: list index out of range
+    """
     for i in range(N - 1):
         for j in range(N - i - 1):
             if a[j] > a[j + 1]:
@@ -33,4 +43,7 @@ def bubble_sort(a, N):
     print(a)
 
 
-main()
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
